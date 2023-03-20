c9ea2603d97aa0dc4f649737c4665bb85b8be47d
ReggieMlay <reggiereal7@gmail.com>
2023-03-20 14:29:43 -0500

    added dark mode feature

--- app/src/main/java/com/example/lumberjackrewards/Settings.java
+++ app/src/main/java/com/example/lumberjackrewards/Settings.java
@@ -2,14 +2,48 @@ package com.example.lumberjackrewards;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.appcompat.widget.SwitchCompat;
 
-public class Settings extends Activity {
+public class Settings extends AppCompatActivity {
+    private SwitchCompat a;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+        if(AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES){
+            setTheme(R.style.DarkTheme);
+        }
+        else{
+            setTheme(R.style.Theme_LumberjackRewards);
+        }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_page);
-    }
-}
+        a = findViewById(R.id.mode);
+        if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
+            a.setChecked(true);
+        }
+            a.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    if (isChecked) {
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    } else {
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    }
+                }
+            });
+        }
+        private void reset()
+        {
+            /*Intent intent = new Intent(getApplicationContext(), Settings.class);
+            //startActivity(intent);
+            finish();*/
+        }
 
+}
