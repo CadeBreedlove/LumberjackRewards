7aff4910cb3cdf8a8d1fef3c7dc0eb14b17cbb18
jfglenn <jessieglenn2002@gmail.com>
2023-03-20 22:03:03 -0500

    reorganized android manifest, registration of new user into db works, set startup as registration page

--- app/src/main/java/com/example/lumberjackrewards/RegistrationActivity.java
+++ app/src/main/java/com/example/lumberjackrewards/RegistrationActivity.java
@@ -3,6 +3,7 @@ package com.example.lumberjackrewards;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -71,12 +72,13 @@ public class RegistrationActivity extends AppCompatActivity {
                         public void onComplete(@NonNull Task<AuthResult> task) {
 
                             if(task.isSuccessful()){
-                                Intent intent = new Intent(RegistrationActivity.this, MainActivity.class);
+                                Intent intent = new Intent(RegistrationActivity.this, BadgesActivity.class);
                                 startActivity(intent);
                                 finish();
                             } else{
                                 String error = task.getException().toString();
                                 Toast.makeText(RegistrationActivity.this, "Registration failed: " + error, Toast.LENGTH_SHORT).show();
+                                Log.d("REGISTER", "Registration failed: " + error);
                             }
                             //loader.dismiss();
                         }
