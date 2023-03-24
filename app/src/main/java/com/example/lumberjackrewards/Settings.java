d40edd6e807a8773d7a02c5919d46dc1ede6e8cb
jfglenn <jessieglenn2002@gmail.com>
2023-03-24 14:32:00 -0500

    added id to logout imageview and renamed btn to logoutBtn on settings.java

--- app/src/main/java/com/example/lumberjackrewards/Settings.java
+++ app/src/main/java/com/example/lumberjackrewards/Settings.java
@@ -6,23 +6,21 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
-public class Settings extends Activity {
 import com.google.firebase.auth.FirebaseAuth;
-
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 public class Settings extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_page);
 
-        Button backBtn = (Button)findViewById(R.id.backButton);
         ImageView logoutBtn = (ImageView)findViewById(R.id.logoutButton);
-        Button btn = (Button)findViewById(R.id.backButton);
+        Button backBtn = (Button)findViewById(R.id.backButton);
         ImageButton btnTextSize = (ImageButton) findViewById(R.id.searchImageButton);
         ImageButton btnSecurity_Privacy = (ImageButton) findViewById(R.id.securityPrivacyButton);
 
@@ -49,7 +47,7 @@ public class Settings extends AppCompatActivity {
             return true;
         });
 
-        btn.setOnClickListener(new View.OnClickListener() {
+        backBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(Settings.this, MainActivity.class));
